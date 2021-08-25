2032a23547a80557b806c71432d0ae9d9ac2a558
Mark Raynsford <code@io7m.com>
2021-08-25 11:19:21 +0000

    Initial testing app

---
+++ org.librarysimplified.deploytest.app/src/main/java/org/librarysimplified/deploytest/app/BreakoutApplication.kt
@@ -0,0 +1,14 @@
+package org.librarysimplified.deploytest.app
+
+import android.app.Application
+import org.slf4j.LoggerFactory
+
+class BreakoutApplication : Application() {
+
+    private val logger = LoggerFactory.getLogger(BreakoutApplication::class.java)
+
+    override fun onCreate() {
+        super.onCreate()
+        this.logger.debug("starting")
+    }
+}
